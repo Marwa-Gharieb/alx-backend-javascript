860ca99945daed3ce58bb5e17e2cae382c4e23ab
marwa <Ifonll>
2024-09-20 18:49:09 -0400

    :)

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,7 +1,6 @@
 /**
  * Displays a message on the STDOUT.
  * @param {String} msg The message to display.
- * @author Bezaleel Olakunori <https://github.com/B3zaleel>
  */
 function displayMessage(input){
     console.log(input);
